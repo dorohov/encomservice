28af7d3a4bb44a3639e399d95646f610fb056487
caezar47 <iss47@ya.ru>
2016-08-09 16:29:57 +0300

    add sites

---
+++ src/block/site/default/head/.document-ready.js
@@ -0,0 +1,92 @@
+$('img').addClass('img-responsive');
+$('.text-block ul').addClass('ul-site');
+var url = window.location.pathname;
+$('.navbar-nav a[href="'+url+'"]').parent().addClass('active'); 
+if(url != "/"){
+	$('<li><a href="/">Главная</a></li>').prependTo($('.navbar-nav'));
+}
+$('._ip__slider').owlCarousel({
+	margin: 0,
+	loop: true,
+	items:1,
+	dots: true,
+	navText: [],
+	autoplay:true,
+	autoplayTimeout:10000,
+	autoplayHoverPause:true ,
+	responsive:{
+	   	0:{nav: false},
+	   	768:{nav: true}
+
+	} 
+}); 
+$('._ipn__carousel').owlCarousel({
+	margin: 50,
+	navText: [],
+	responsive:{
+	    0:{
+	        items:1,
+			dots: true,
+			nav: false,
+	    },
+	    480:{
+	        items:2,
+			dots: true,
+			nav: false,
+	    },
+	   	768:{
+	        items:3,
+			dots: true,
+			nav: false,
+	    },
+	    1025:{
+	        items:4,
+			dots: false,
+			nav: true
+	    }
+	}
+});
+$('._cip__owl').owlCarousel({
+	margin: 20,
+	navText: [],
+	responsive:{
+	    0:{
+	        items:1,
+			dots: true,
+			nav: false,
+	    },
+	    480:{
+	        items:2,
+			dots: true,
+			nav: false,
+	    },
+	   	768:{
+	        items:3,
+			dots: true,
+			nav: false,
+	    },
+	    1025:{
+	        items:3,
+			dots: false,
+			nav: true
+	    }
+	}
+});
+$('.fancybox').fancybox();
+// конструкция для активации fancybox <a class="fancybox-buttons" data-fancybox-group="button" href="" data-title=""><img src="" alt="" /></a>
+$('.fancybox-buttons').fancybox({
+	openEffect  : 'none',
+	closeEffect : 'none',
+	prevEffect : 'none',
+	nextEffect : 'none',
+	closeBtn  : false,
+	helpers : {
+		title : {
+			type : 'inside'
+		},
+		buttons	: {}
+	},
+	afterLoad : function() {
+		this.title = this.title;
+	}
+});
